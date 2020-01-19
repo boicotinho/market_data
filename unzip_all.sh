6f29a98e637005532662544bc2c4bb491ece3335
Fabio <fabio@taniustech.com>
2020-01-19 14:59:20 -0800

    unzip command

---
+++ unzip_all.sh
@@ -0,0 +1,2 @@
+cat 20170703-7.dat.zip.aa 20170703-7.dat.zip.ab 20170703-7.dat.zip.ac 20170703-7.dat.zip.ad 20170703-7.dat.zip.ae 20170703-7.dat.zip.af 20170703-7.dat.zip.ag > 20170703-7.dat.zip
+unzip 20170703-7.dat.zip && rm 20170703-7.dat.zip && chmod -xw 20170703-7.dat
